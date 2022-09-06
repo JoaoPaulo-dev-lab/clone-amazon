ae8845e884c1136063736dcd87b3277ae0aaeed8
João Paulo <paulo.j7.jp89@gmail.com>
2022-09-06 18:38:33 -0300

    Alteracao nos banners

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,12 @@
     <MoreMovies :banners="banners1"/>
     <div class="span">Mais assistidos</div>
     <MoreMovies :banners="banners2"/>
-    <div class="span">Mais assistidos</div>
-    <MoreMovies :banners="banners2"/>
-    <div class="span">Mais assistidos</div>
-    <MoreMovies :banners="banners2"/>
+    <div class="span">Filmes recomendados</div>
+    <MoreMovies :banners="banners3"/>
+    <div class="span">Melhores filmes</div>
+    <MoreMovies :banners="banners4"/>
+    <div class="span">Filmes de comédia</div>
+    <MoreMovies :banners="banners5"/>
   </div>
 </template>
 
@@ -87,13 +89,118 @@ const banners2 = [
         bannerURL: 'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/59898e3b729756b7e12e43f511ffa9c4446745890d644e96d630cc85a7c49f4f._UY500_UX667_RI_V_TTW_.jpg'
     }
 ]
+const banners3 = [
+    {
+        id: 1,
+        bannerURL: 'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/e85435452c687826f4898a4e790ca563a0be960ff4d7e2b557e8f032f49e15a7._UY500_UX667_RI_V_TTW_.png'
+    },
+    {
+        id: 2,
+        bannerURL: 'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/adb49794a3877207783f1fbccad15964bdf4209c1f72b7f758f12ddd74f24c33._UY500_UX667_RI_V_TTW_.jpg'
+    },
+    {
+        id: 3,
+        bannerURL: 'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/e6455a8504e4292716f8ad0d2c02e04509dd6e86be55cc1125df9b877bb89f04._UY500_UX667_RI_V_TTW_.jpg'
+    },
+    {
+        id: 4,
+        bannerURL: 'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/6c122a28cbcb4f3433f4bc47dd3041a3234a8774d0c999f0641080a2fda9d059._UY500_UX667_RI_V_TTW_.jpg'
+    },
+    {
+        id: 5,
+        bannerURL: 'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/a02eab0a2ffd89593dc621c66b531dd59980c89fd81033bc61674429043426aa._UY500_UX667_RI_V_TTW_.jpg'
+    },
+    {
+        id: 6,
+        bannerURL: 'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/1cad8b21e10a068bd9dfa9e16503fae899e53d037557697a7bf0b910aa012820._UY500_UX667_RI_V_TTW_.jpg'
+    },
+    {
+        id: 7,
+        bannerURL: 'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/17de976aa3eef0ae588a0cfcfd920aa35a0541e5c41d27d30485e21d0c89fba8._UY500_UX667_RI_V_TTW_.jpg'
+    },
+    {
+        id: 8,
+        bannerURL: 'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/f57469e0cb35f51aef483e2c5aa00ca8ed0b33363012eca2656dcb67c2b98eea._UY500_UX667_RI_V_TTW_.jpg'
+    }
+]
+const banners4 = [
+    {
+        id: 1,
+        bannerURL: 'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/fbd9262f76f6feb91d05abea759f58644eb54f4ef8ff1a07e64a7908c8620eb0._UY500_UX667_RI_V_TTW_.jpg'
+    },
+    {
+        id: 2,
+        bannerURL: 'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/569abce1202d4157423e384bd7a5fb1efce2c8399bf6125703edc708e56d2ec6._UY500_UX667_RI_V_TTW_.jpg'
+    },
+    {
+        id: 3,
+        bannerURL: 'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/7627b4bc590d30635ab63835635282e4ff8a98d52c9c7b76a93cd2e79336db5c._UY500_UX667_RI_V_TTW_.jpg'
+    },
+    {
+        id: 4,
+        bannerURL: 'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/11c72dd35223e9a4f5e460f975e4a10fbc469da53a08c4830a3e99d33b457bb8._UY500_UX667_RI_V_TTW_.jpg'
+    },
+    {
+        id: 5,
+        bannerURL: 'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/8e791dfabd3deb6241cf667ba1b8f57bda1d8a5a36a1e22cc0dcf41a28205eb2._UY500_UX667_RI_V_TTW_.jpg'
+    },
+    {
+        id: 6,
+        bannerURL: 'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/8cc15cef464265a8ea09d0cfe85ce375ca1e6cc5f93fe8e679ccd8169e9ba75b._UY500_UX667_RI_V_TTW_.jpg'
+    },
+    {
+        id: 7,
+        bannerURL: 'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/699e933942eda51a1883b31e51496b898d3475d972f25cdb2d36f3fbb8642500._UY500_UX667_RI_V_TTW_.jpg'
+    },
+    {
+        id: 8,
+        bannerURL: 'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/079300a17c59a1b73d2f1efbfe87438eca3e42b18fa89deacb24a123b5aae9a0._UY500_UX667_RI_V_TTW_.jpg'
+    }
+]
+const banners5 = [
+    {
+        id: 1,
+        bannerURL: 'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/9d4b5caf650861be0ba1a8cecb4bf32c2daf3856b86b6979a9bbe36cec0b5e3d._UY500_UX667_RI_V_TTW_.jpg'
+    },
+    {
+        id: 2,
+        bannerURL: 'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/b9d7db5a177438cfd8b82ad39a0faeae2e36b81427992290e43f27d83a48718c._UY500_UX667_RI_V_TTW_.jpg'
+    },
+    {
+        id: 3,
+        bannerURL: 'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/a7b76ec9e674a82a41cfdf1fccc2011ed6b6149ee3943fa664a896090cac5054._UY500_UX667_RI_V_TTW_.jpg'
+    },
+    {
+        id: 4,
+        bannerURL: 'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/be5140cf94089854654828b4174608aa30b95c23dbd03293df54eb60c4b94f82._UY500_UX667_RI_V_TTW_.jpg'
+    },
+    {
+        id: 5,
+        bannerURL: 'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/02622f1559c2e654718e4c7736731536d20215f3c77ec1b2dfc9a494aea2f69a._UY500_UX667_RI_V_TTW_.jpg'
+    },
+    {
+        id: 6,
+        bannerURL: 'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/69af9f1f61fa4338b2a038b8ab0fef8f91ec6272ace09f8571edc03779680c37._UY500_UX667_RI_V_TTW_.jpg'
+    },
+    {
+        id: 7,
+        bannerURL: 'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/e81379689d8ada79a7d9eb5ed54cfc3bb20325464f5e0362f47122a19ec9209a._UY500_UX667_RI_V_TTW_.jpg'
+    },
+    {
+        id: 8,
+        bannerURL: 'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/7aef0e85be40b55a6a16bdee18917efaa640bdacf4b74d7abb7f90b125c65cb7._UY500_UX667_RI_V_TTW_.jpg'
+    }
+]
 
 export default {
     name: 'App',
     data(){
       return{
         banners1,
-        banners2
+        banners2,
+        banners3,
+        banners4,
+        banners5
       }
     },
     components: { HeaderTest, BannerTest, MoreMovies }
